b1802367b571360b7019f729d3a02c513b00bc11
castastrophe <cassondrar+github@adobe.com>
2022-12-21 15:26:50 -0500

    chore: remove dist from main

--- dist/utilities.d.ts
+++
@@ -1,5 +0,0 @@
-import type { NodePlopAPI } from 'node-plop';
-export declare const determinePath: (destination: string | undefined, data: Record<string, any>, renderString: NodePlopAPI['renderString']) => string;
-export declare const determineFallback: (userInput: string | undefined, data: Record<string, any>, renderString: NodePlopAPI['renderString']) => string | undefined;
-export declare const yarnFetchVersion: (pkgName: string) => string;
-export declare const npmFetchVersion: (pkgName: string) => string;
